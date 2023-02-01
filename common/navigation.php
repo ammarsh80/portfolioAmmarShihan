af29b0fcdeb348da589f7ebdd0e743b8bd8d2df6
Ammar_Shihan <mmarsh80@hotmail.com>
2023-02-01 12:17:35 +0100

    ajout burger menu

--- common/navigation.php
+++ common/navigation.php
@@ -1,11 +1,47 @@
 <div id="div_nav">
     <nav class="nav">
-        <a href="index.php?page=accueil" class="a_logo">
-            <img src="./assets/img/logo.PNG" class="logo" alt="logo-portfolio">
-        </a>
-        <a href="index.php?page=accueil">Accueil</a>
-        <a href="index.php?page=realisations">Réalisations</a>
-        <a href="index.php?page=veilles_technologiques">Veilles Technologiques</a>
-        <a href="index.php?page=cv_Ammar_Shihan">C.V</a>
+        
+        <!-- <label for="hidden" class="burger_menu"></label>
+        <input type="checkbox" name="hidden" id="hidden" />
+        <div class="invert_border_right"></div>
+        <div class="invert_border_left"></div> -->
+        <div class="div_nav_flex"> 
+            <a href="index.php?page=accueil" class="a_logo">
+                <img src="./assets/img/logo.PNG" class="logo" alt="logo-portfolio">
+            </a>
+            <ul id="pages" class="list_nav">
+                <li class="bar bar1 bye smallScreen"><a href="index.php?page=accueil">Accueil </a></li>
+                <li class="bar bar2 bye smallScreen"><a href="index.php?page=realisations">Réalisations </a></li>
+                <li class="bar bar3 bye smallScreen"><a href="index.php?page=veilles_technologiques">Veilles Technologiques </a></li>
+                <li class="bar bar4 bye smallScreen"><a href="index.php?page=cv_Ammar_Shihan">C.V </a></li>
+
+            </ul>
+        </div>
+        <div class="box">
+                    <div class="cont-lignes btn1">
+                        <div class="lignes"></div>
+                        <div class="lignes"></div>
+                        <div class="lignes"></div>
+                    </div>
     </nav>
-</div>
+   </div>
+<!-- <nav>
+                    <ul class="list_nav">
+                        <li class="bar"><a href="index.php?page=accueil">Alexandre Bozzo</a></li>
+                        <li class="bar bye smallScreen"><a href="index.php?page=projet">Projets</a></li>
+                        <li class="bar bye smallScreen"><a href="index.php?page=veille">Veille Technologique</a></li>
+                        <li class="bar bye smallScreen"><a href="index.php?page=cv">CV</a></li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <div class="cont-lignes btn1">
+                        <div class="lignes"></div>
+                        <div class="lignes"></div>
+                        <div class="lignes"></div>
+                    </div>
+</nav> -->
+
+<!-- <a href="index.php?page=accueil">Accueil</a>
+<a href="index.php?page=realisations">Réalisations</a>
+<a href="index.php?page=veilles_technologiques">Veilles Technologiques</a>
+<a href="index.php?page=cv_Ammar_Shihan">C.V</a> -->
--- js/main.js
+++ js/main.js
@@ -21,3 +21,13 @@ function topFunction() {
     behavior: 'smooth' 
   });
 }
+
+let btn = document.querySelector(".btn1");
+let list_nav = document.querySelector(".list_nav");
+let bye = document.querySelectorAll(".bye");
+btn.addEventListener("click", (e) => {
+  btn.classList.toggle("active");
+  bye.forEach((element) => {
+    element.classList.toggle("smallScreen");
+  });
+});
